d738caf5c50ed221ff6580fc6c45ab32974288f1
Almir Dias <156623370+Almir-Dias@users.noreply.github.com>
2024-09-06 17:35:57 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Talles Magno",
+        descricao : "Talles Magno é um jovem atacante que tem se destacado no Corinthians. Conhecido por sua velocidade e habilidade, ele já marcou gols importantes e se tornou um dos principais jogadores do time.",
+        link : "https://pt.wikipedia.org/wiki/Talles_Magno",
+        tags: "jove atacante corinthians habilidade velocidade"
+    },
+    
+    {
+        titulo: "Fagner",
+        descricao: "Fagner é um lateral-direito experiente e capitão do Corinthians. Conhecido por sua força física, cruzamentos precisos e liderança em campo, ele é peça fundamental na equipe.",
+        link: "https://pt.wikipedia.org/wiki/Fagner_(futebolista)",
+        tags: "capitão capitao força cruzamento liderança experiente corinthians"
+    },
+    
+    {
+        titulo: "Hugo Souza",
+        descricao: "Hugo Souza é um goleiro que se transferiu para o Corinthians para disputar posição. Conhecido por suas defesas difíceis e reflexos rápidos, ele busca se firmar como titular da equipe.",
+        link: "https://pt.wikipedia.org/wiki/Hugo_Souza",
+        tags: "goleiro defesas reflexos titular corinthians"
+    }
+];
+
